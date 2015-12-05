8db1e88baf155aba7e2612a94eef2442182ad94e
RamanBut-Husaim <raman_but-husaim@live.com>
2015-12-05 18:23:25 +0300

    [Queryable] Make changes to unit-test project.

--- Queryable.Example/Queryable.Example.Tests/E3SProviderTests.cs
+++ Queryable.Example/Queryable.Example.Tests/E3SProviderTests.cs
@@ -1,23 +1,30 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Linq;
 using Queryable.Example.E3SClient;
 using Queryable.Example.E3SClient.Entities;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Queryable.Example.Tests
 {
     public class E3SProviderTests
     {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public E3SProviderTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
         [Fact]
         public void WithoutProvider()
         {
             var client = new E3SQueryClient(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
-            var res = client.SearchFTS<EmployeeEntity>("workstation:(EPRUIZHW0060)", 0, 1);
+            var res = client.SearchFTS<EmployeeEntity>("workstation:(EPBYMINW3594)", 0, 1);
 
             foreach (var emp in res)
             {
-                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+                _outputHelper.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
             }
         }
 
@@ -25,23 +32,22 @@ namespace Queryable.Example.Tests
         public void WithoutProviderNonGeneric()
         {
             var client = new E3SQueryClient(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
-            var res = client.SearchFTS(typeof(EmployeeEntity), "workstation:(EPRUIZHW0060)", 0, 10);
+            var res = client.SearchFTS(typeof(EmployeeEntity), "workstation:(EPBYMINW3594)", 0, 10);
 
             foreach (var emp in res.OfType<EmployeeEntity>())
             {
-                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+                _outputHelper.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
             }
         }
-
 
         [Fact]
         public void WithProvider()
         {
             var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
 
-            foreach (var emp in employees.Where(e => e.workstation == "EPRUIZHW0060"))
+            foreach (var emp in employees.Where(e => e.workstation == "EPBYMINW3594"))
             {
-                Console.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
+                _outputHelper.WriteLine("{0} {1}", emp.nativename, emp.startworkdate);
             }
         }
     }

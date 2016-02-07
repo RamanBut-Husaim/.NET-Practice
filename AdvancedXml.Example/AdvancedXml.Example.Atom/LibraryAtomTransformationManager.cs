c5ae3eca0aa31ad0ce1c54c61ef7f9b17def9c5d
Raman But-Husaim <raman_buthusaim@yahoo.com>
2016-02-07 11:19:19 +0300

    [AdvancedXml] Start manager implementation.

---
+++ AdvancedXml.Example/AdvancedXml.Example.Atom/LibraryAtomTransformationManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace AdvancedXml.Example.Atom
+{
+    public sealed class LibraryAtomTransformationManager : IDisposable
+    {
+        private readonly Lazy<Stream> _resourceStream;
+        private bool _disposed;
+
+        public LibraryAtomTransformationManager()
+        {
+            _resourceStream = new Lazy<Stream>(
+                () => Assembly
+                    .GetExecutingAssembly()
+                    .GetManifestResourceStream("AdvancedXml.Example.Atom." + "LibraryAtomTransformation.xslt"));
+        }
+
+        public void Transform(string inputFile, string outputFile)
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("manager");
+            }
+
+            var compiledTransform = new XslCompiledTransform();
+            var settings = new XsltSettings {EnableScript = true};
+
+            _resourceStream.Value.Seek(0, SeekOrigin.Begin);
+
+            using (var transformation = XmlReader.Create(_resourceStream.Value))
+            {
+                compiledTransform.Load(transformation);
+
+                compiledTransform.Transform(inputFile, outputFile);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_resourceStream.IsValueCreated)
+                    {
+                        _resourceStream.Value.Dispose();
+                    }
+
+                    _disposed = true;
+                }
+            }
+        }
+    }
+}

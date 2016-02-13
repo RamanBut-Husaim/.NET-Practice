502c8ab6677d0ce98402952b73058fa2bfd4e645
Raman But-Husaim <raman_buthusaim@yahoo.com>
2016-02-13 18:11:09 +0300

    [Debugging] Add key generator.

---
+++ Debugging.Example.KeyGenerator/KeyGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace Debugging.Example.KeyGenerator
+{
+    public sealed class KeyGenerator
+    {
+        private readonly NetworkInterface _networkInterface;
+
+        public KeyGenerator()
+        {
+            _networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+        }
+
+        public string Generate()
+        {
+            if (_networkInterface == null)
+            {
+                throw new InvalidOperationException("The network interface is not specified");
+            }
+
+            byte[] addressBytes = _networkInterface.GetPhysicalAddress().GetAddressBytes();
+            byte[] dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            var password = addressBytes
+                .Select((value, index) => value ^ dateBytes[index])
+                .Select(value =>
+                {
+                    if (value < 999)
+                    {
+                        return value * 10;
+                    }
+
+                    return value;
+                })
+                .Select(value => value.ToString(CultureInfo.InvariantCulture))
+                .Aggregate(string.Empty, (accumulator, value) => accumulator + value + "-");
+
+            return password.Substring(0, password.Length - 1);
+        }
+    }
+}
---
+++ Debugging.Example.KeyGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Debugging.Example.KeyGenerator
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var keyGenerator = new KeyGenerator();
+
+            Console.WriteLine("Key: {0}", keyGenerator.Generate());
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}

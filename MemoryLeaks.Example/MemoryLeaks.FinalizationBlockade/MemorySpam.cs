a7d06d4667bdc1c0e04aa96ea8ee1ce00f71b753
Raman But-Husaim <raman_buthusaim@yahoo.com>
2016-02-27 23:11:10 +0300

    [Profiling] Memory leaks. Finalization blockade.

---
+++ MemoryLeaks.Example/MemoryLeaks.FinalizationBlockade/MemorySpam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MemoryLeaks.FinalizationBlockade
+{
+    public sealed class MemorySpam : IDisposable
+    {
+        private readonly byte[] _bigSpam;
+
+        public MemorySpam()
+        {
+            _bigSpam = new byte[1024 * 1024];
+        }
+
+        ~MemorySpam()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ MemoryLeaks.Example/MemoryLeaks.FinalizationBlockade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Management;
+using System.Threading;
+
+namespace MemoryLeaks.FinalizationBlockade
+{
+    public sealed class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var obj = new ManagementObject("win32_logicaldisk.deviceid=\"C:\"");
+            // There is an issue related to COM object wrapper finalization.
+            // They should be finalized on the same thread. At this point
+            // we call a function that creates a COM object inside. And actually
+            // it should be disposed as no longer used.
+            obj.Get();
+
+            while (true)
+            {
+                // These objects are finalizable as well but finalization thread
+                // destroys objects one by one and COM object is the first candidate.
+                new MemorySpam();
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(10));
+            }
+        }
+    }
+}
